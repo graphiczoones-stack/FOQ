14767c557c6232c181537990dc95182a342ab89e
graphiczoones-stack <graphiczoones@gmail.com>
2026-01-22 20:49:18 +0200

    Fix: move about page to public layout group

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Features } from "@/components/home/Features";
-
-export default function AboutPage() {
-    return (
-        <main className="min-h-screen pt-32">
-            <Features />
-        </main>
-    );
-}
